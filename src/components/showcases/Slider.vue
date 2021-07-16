bc91e1a63235beb8cc994a411c03797cc2388867
Burak <wizard1456@hotmail.com>
2021-07-16 22:30:29 +0300

    Slider img lazy loading

--- src/components/showcases/Slider.vue
+++ src/components/showcases/Slider.vue
@@ -5,13 +5,25 @@
         <div @click="toContent(item)" class="click">
           <v-carousel-item
             v-if="item.showcases.includes('Carousel')"
-            :src="item.image_path"
             reverse-transition="fade-transition"
           >
             <div class="carousel-title">
               <h3>{{ item.name }}</h3>
               <span>{{ item.short_description }}</span>
             </div>
+            <v-img
+              :src="item.image_path"
+              cover
+            >
+              <template #placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
           </v-carousel-item>
         </div>
       </div>
@@ -29,9 +41,9 @@ export default {
     },
   },
   methods: {
-    toContent(item){
-      this.$emit("content", item)
-    }
+    toContent(item) {
+      this.$emit("content", item);
+    },
   },
 };
 </script>
@@ -42,7 +54,7 @@ export default {
   background-color: #00000049;
   width: auto;
 }
-.click{
+.click {
   cursor: pointer;
 }
 </style>
